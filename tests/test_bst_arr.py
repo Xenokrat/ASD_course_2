c82cc581c92a6c8a4371bfd2021527b87a9e6456
Xenokrat <ksenokrat@gmail.com>
2022-11-04 18:29:17 +0300

    feat 4: add methods for creating test tree

--- tests/test_bst_arr.py
+++ tests/test_bst_arr.py
@@ -5,6 +5,11 @@ from binary_search_array import aBST
 
 class TestaBST(unittest.TestCase):
     def test_tree_array_init(self):
+    def create_test_tree(self) -> aBST:
+        tree = aBST(3)
+        tree.Tree = [50, 25, 75, None, 37, 62, 84,
+                     None, None, 31, 43, 55, None, None, 92]
+        return tree
         t1 = aBST(0)
         t2 = aBST(1)
         t3 = aBST(2)
