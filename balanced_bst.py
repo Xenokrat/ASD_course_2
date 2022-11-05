28893b6434e8003a36b0e9aad9969bc90902ab84
Xenokrat <ksenokrat@gmail.com>
2022-11-05 17:47:48 +0300

    feat 5: add GenerateBBSTArray() function

---
+++ balanced_bst.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+def GenerateBBSTArray(a: List[int]) -> List[int]:
+    if not a:
+        return []
+
+    a.sort()
+    root = _get_root(a)
+    return [root] + _generate_bbst_array(a)
+
+
+def _generate_bbst_array(array: List[int]) -> List[int]:
+    """Recursivly return left / right root's children"""
+
+    mid = len(array) // 2
+    if mid == 0:
+        return []
+
+    left_array = array[:mid]
+    right_array = array[mid + 1:]
+
+    left_child = _get_root(left_array)
+    right_child = _get_root(right_array)
+
+    return (
+        [left_child, right_child]
+        + _generate_bbst_array(array[:mid])
+        + _generate_bbst_array(array[mid + 1:])
+    )
+
+
+def _get_root(array: List[int]) -> int:
+    """Return root as center element of array"""
+
+    return array[len(array) // 2]
