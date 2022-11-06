b8fc3ed9f18ffd82226a93b37b794fd0cc52b0fd
Mantis <barabanovpv95@gmail.com>
2022-11-06 14:05:57 +0300

    feat 6: add GenerateTree implementation

--- balanced_node_bst.py
+++ balanced_node_bst.py
@@ -27,8 +27,34 @@ class BalancedBST:
     def __init__(self) -> None:
         self.Root = None
 
-    def GenerateTree(self, a: List[int]):
+    def GenerateTree(self, a: List[int]) -> None:
         """Create a tree from non-sorted array 'a'"""
+
+        a.sort()
+        self.Root = self._generate_tree(a, None)
+
+    def _generate_tree(self,
+                       array: List[int],
+                       parent: Optional[BSTNode]) -> Optional[BSTNode]:
+
+        # base case
+        if not array:
+            return None
+
+        # create new BSTNode (root) with current key as a
+        # mid-array value, set parent node from F() param
+        mid = len(array) // 2
+        key = array[mid]
+        root = BSTNode(key, parent)
+
+        # recursively call F() with [:mid] slice and
+        # root as parent node, set root.LeftChild to F() result
+        root.LeftChild = self._generate_tree(array[:mid], root)
+
+        # same with RightChild
+        root.RightChild = self._generate_tree(array[mid + 1:], root)
+
+        return root
 
     def IsBalanced(self, root_node: BSTNode) -> bool:
         """Check if tree part from root_node is balanced"""
