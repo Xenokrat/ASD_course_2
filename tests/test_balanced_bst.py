3de24d19339dbe173a44bb03553c0a43df2c9d5a
Mantis <barabanovpv95@gmail.com>
2022-11-06 09:17:50 +0300

    feat 5: add more trees to test

--- tests/test_balanced_bst.py
+++ tests/test_balanced_bst.py
@@ -8,15 +8,19 @@ class TestaBST(unittest.TestCase):
     def test_get_balanced_array(self) -> None:
         test_list = list(range(1, 8))
         shuffle(test_list)
-        balances_list = GenerateBBSTArray(test_list)
-        self.assertListEqual(balances_list, [4, 2, 6, 1, 3, 5, 7])
+        balanced_list = GenerateBBSTArray(test_list)
+        self.assertListEqual(
+            balanced_list,
+            [4, 2, 6, 1, 3, 5, 7]
+        )
 
     def test_get_balanced_array2(self) -> None:
-        test_list = [37, 55, 62, 50, 25, 15, 22, 92, 80, 84, 68, 31, 75, 43]
-        balances_list = GenerateBBSTArray(test_list)
+        test_list = list(range(15))
+        shuffle(test_list)
+        balanced_list = GenerateBBSTArray(test_list)
         self.assertListEqual(
-            balances_list,
-            [50, 25, 75, 20, 37, 62, 84, 15, 22, 31, 43, 55, 68, 80, 92]
+            balanced_list,
+            [7, 3, 11, 1, 5, 9, 13, 0, 2, 4, 6, 8, 10, 12, 14]
         )
 
     def test_empty_array(self) -> None:
