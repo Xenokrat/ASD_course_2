bfbb2ac7c0d7fea5090b3fe00fe2756376e745cf
Mantis <barabanovpv95@gmail.com>
2022-11-11 09:49:27 +0300

    feat 8: add tests for EvenTrees method

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -119,6 +119,40 @@ class TestTrees(unittest.TestCase):
         empty_tree = SimpleTree(None)
         self.assertEqual(empty_tree.LeafCount(), 0)
 
+    def test_even_tree(self) -> None:
+        node_1 = SimpleTreeNode(1, None)
+        tree = SimpleTree(node_1)
+        node_2 = SimpleTreeNode(2, None)
+        node_5 = SimpleTreeNode(5, None)
+        node_7 = SimpleTreeNode(7, None)
+        node_3 = SimpleTreeNode(3, None)
+        node_6 = SimpleTreeNode(6, None)
+        node_4 = SimpleTreeNode(6, None)
+        node_8 = SimpleTreeNode(6, None)
+        node_9 = SimpleTreeNode(6, None)
+        node_10 = SimpleTreeNode(6, None)
+        tree.AddChild(tree.Root, node_2)
+        tree.AddChild(tree.Root, node_3)
+        tree.AddChild(tree.Root, node_6)
+        tree.AddChild(node_2, node_5)
+        tree.AddChild(node_2, node_7)
+        tree.AddChild(node_3, node_4)
+        tree.AddChild(node_6, node_8)
+        tree.AddChild(node_8, node_9)
+        tree.AddChild(node_8, node_10)
+
+        self.assertListEqual(
+            tree.EvenTrees(),
+            [node_1, node_3, node_1, node_6]
+        )
+
+    def test_even_tree2(self) -> None:
+        tree, nodes = create_test_tree()
+        self.assertListEqual(
+            tree.EvenTrees(),
+            []
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
